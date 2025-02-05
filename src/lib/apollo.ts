0768a8d08bfc724e78c8f51e78e20beb6cd73d3a
sercul <sercul@gmail.com>
2025-02-05 09:58:28 +0100

    command changed

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: process.env.NEXT_PUBLIC_API_URL || 'https://gominga-api.onrender.com/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
